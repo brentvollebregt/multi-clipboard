bbec59c5ab7357beb3337c7146234d80f0c09a7e
Brent Vollebregt <volleys2468@gmail.com>
2017-07-01 18:13:25 +1200

    Removed exit() in main script and replaced with an if else

--- clipboards.py
+++ clipboards.py
@@ -7,39 +7,38 @@ data = clipboard_assist.getData()
 
 if len(sys.argv) < 2:
     print ("Usage: python clipboards.py {switch, clear, view}")
-    exit()
-
-if sys.argv[1] == "switch":
-    if len(sys.argv) == 3:
-        clipboard = clipboard_assist.cleanString(sys.argv[2])
-        clipboard_assist.saveClipboard(clipboards_location, data)
-        clipboard_assist.loadClipboard(clipboards_location, clipboard)
-        data["current_clipboard"] = clipboard
-        clipboard_assist.setData(data)
-    else:
-        print ("Usage: python clipboards.py switch clipboard")
-
-elif sys.argv[1] == "clear":
-    if len(sys.argv) == 2:
-        for clipboard in os.listdir(clipboards_location):
-            clipboard_assist.clear(clipboards_location, clipboard[:-4])
-        data["current_clipboard"] = '1'
-        clipboard_assist.setData(data)
-    elif len(sys.argv) == 3:
-        clipboard = clipboard_assist.cleanString(sys.argv[2])
-        if not clipboard_assist.clear(clipboards_location, clipboard):
-            print ("Clipboard " + clipboard + " does not exist")
-    else:
-        print ("Usage: python clipboards.py clear [clipboard]")
-
-elif sys.argv[1] == "view":
-    if len(sys.argv) == 2:
-        clipboard_assist.view(clipboards_location)
-    elif len(sys.argv) == 3:
-        if not clipboard_assist.view_single(clipboards_location, sys.argv[2]):
-            print ("Clipboard " + sys.argv[2] + " does not exist")
-    else:
-        print ("Usage: python clipboards.py view [clipboard]")
-
 else:
-    print ("Usage: python clipboards.py {switch, clear, view}")
+    if sys.argv[1] == "switch":
+        if len(sys.argv) == 3:
+            clipboard = clipboard_assist.cleanString(sys.argv[2])
+            clipboard_assist.saveClipboard(clipboards_location, data)
+            clipboard_assist.loadClipboard(clipboards_location, clipboard)
+            data["current_clipboard"] = clipboard
+            clipboard_assist.setData(data)
+        else:
+            print ("Usage: python clipboards.py switch clipboard")
+
+    elif sys.argv[1] == "clear":
+        if len(sys.argv) == 2:
+            for clipboard in os.listdir(clipboards_location):
+                clipboard_assist.clear(clipboards_location, clipboard[:-4])
+            data["current_clipboard"] = '1'
+            clipboard_assist.setData(data)
+        elif len(sys.argv) == 3:
+            clipboard = clipboard_assist.cleanString(sys.argv[2])
+            if not clipboard_assist.clear(clipboards_location, clipboard):
+                print ("Clipboard " + clipboard + " does not exist")
+        else:
+            print ("Usage: python clipboards.py clear [clipboard]")
+
+    elif sys.argv[1] == "view":
+        if len(sys.argv) == 2:
+            clipboard_assist.view(clipboards_location)
+        elif len(sys.argv) == 3:
+            if not clipboard_assist.view_single(clipboards_location, sys.argv[2]):
+                print ("Clipboard " + sys.argv[2] + " does not exist")
+        else:
+            print ("Usage: python clipboards.py view [clipboard]")
+
+    else:
+        print ("Usage: python clipboards.py {switch, clear, view}")
